887ac9dacafc23680fffce8e361d09fead66468e
Kyle Giammarco <kyle@giammar.co>
2024-12-18 18:56:59 -0500

    option regenerate and edit any message

--- Ollamac/Views/Chats/Subviews/UserMessageView.swift
+++ Ollamac/Views/Chats/Subviews/UserMessageView.swift
@@ -11,11 +11,15 @@ import ViewCondition
 struct UserMessageView: View {
     private let windowWidth = NSApplication.shared.windows.first?.frame.width ?? 0
     private let content: String
+    private let identifier: UUID
     private let copyAction: (_ content: String) -> Void
+    private let generateAtAction: (_ identifier: UUID) -> Void
     
-    init(content: String, copyAction: @escaping (_ content: String) -> Void) {
+    init(content: String, identifier : UUID, copyAction: @escaping (_ content: String) -> Void, generateAtAction: @escaping (_ identifier: UUID) -> Void) {
         self.content = content
         self.copyAction = copyAction
+        self.identifier = identifier
+        self.generateAtAction = generateAtAction
     }
     
     var body: some View {
@@ -24,15 +28,15 @@ struct UserMessageView: View {
             
             VStack(alignment: .trailing) {
                 Text(content)
-                    .padding(12)
+                    .padding(8)
                     .background(.accent)
                     .foregroundColor(.white)
                     .textSelection(.enabled)
-                    .font(Font.system(size: 16))
                     .clipShape(RoundedRectangle(cornerRadius: 12))
                 
                 HStack(spacing: 16){
                     MessageButton("Copy", systemImage: "doc.on.doc", action: { copyAction(content) })
+                    MessageButton("Edit", systemImage: "square.and.pencil", action: { generateAtAction(identifier) })
                 }
             }
             .frame(maxWidth: windowWidth / 2, alignment: .trailing)
